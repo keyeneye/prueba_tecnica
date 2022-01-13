c51172be455cf184aa6f9086d23aeb58f61a1b69
keyeneye <david02ing@gmail.com>
2022-01-12 20:45:46 -0600

    validations to add employee

--- src/EmployeeForm/index.tsx
+++ src/EmployeeForm/index.tsx
@@ -1,11 +1,17 @@
 import "./EmployeeForm.css";
 import { FC, useState } from "react";
+import Swal from "sweetalert2";
 
 const EmployeeForm: FC<{
   createEmployee: Function;
   openModal: boolean;
   setOpenModal: Function;
 }> = ({ createEmployee, openModal, setOpenModal }) => {
+  const RegexName: RegExp =
+    /^[a-zA-Z\u00C0-\u024F\u1E00-\u1EFF]{3,60}(([ ][a-zA-Z\u00C0-\u024F\u1E00-\u1EFF ])?[a-zA-Z\u00C0-\u024F\u1E00-\u1EFF ]*)*$/;
+  const EmailRegex: RegExp =
+    /^[a-z0-9!#$%&.'*+/=?^_`{|}~-]+@[a-z0-9-]+(\.[a-z0-9-]{2,})+$/i;
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [telephone, setTelephone] = useState("");
@@ -16,10 +22,46 @@ const EmployeeForm: FC<{
   };
   const onSubmit = (e: any) => {
     e.preventDefault();
+    if (!name) {
+      Swal.fire({
+        icon: "error",
+        title: "Es necesario agregar el nombre",
+      });
+    }
+    if (!email) {
+      Swal.fire({
+        icon: "error",
+        title: "Es necesario agregar el correo",
+      });
+    }
+    if (!telephone) {
+      Swal.fire({
+        icon: "error",
+        title: "Es necesario agregar el telefono",
+      });
+    }
+    if (!file) {
+      Swal.fire({
+        icon: "error",
+        title: "Es necesario agregar una imagen",
+      });
+    }
+    if (!RegexName.test(name)) {
+      Swal.fire({
+        icon: "error",
+        title: "Ingrese un nombre valido",
+      });
+    }
+    if (!EmailRegex.test(email)) {
+      Swal.fire({
+        icon: "error",
+        title: "Ingrese un correo valido",
+      });
+    }
     if (name && email && telephone && file) {
       createEmployee(name, email, telephone, file);
+      setOpenModal(!openModal);
     }
-    setOpenModal(!openModal);
   };
   const setImage = (e: any) => {
     const file = e.target.files[0];
